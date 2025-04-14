70e248830c72aa2d78bf5a478df0afc87dddaca7
erickrocha87 <ericksaggi31@gmail.com>
2025-04-14 19:24:37 -0300

    Implements HttpClient and mock data for view data

--- src/app/components/table-service/table-service.component.ts
+++ src/app/components/table-service/table-service.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { Subscriptions } from '../../model/subscriptions';
 import { CommonModule } from '@angular/common';
 import { SubscriptionsService } from '../../services/subscriptions.service';
+import { Observable } from 'rxjs';
 
 
 @Component({
@@ -12,13 +13,13 @@ import { SubscriptionsService } from '../../services/subscriptions.service';
 })
 export class TableServiceComponent {
 
-  subscriptions: Subscriptions[] = [];
+  subscriptions$: Observable<Subscriptions[]>;
 
   // subscriptionsService: SubscriptionsService;
 
   constructor(private subscriptionsService: SubscriptionsService) {
     // this.subscriptionsService = new SubscriptionsService();
-    this.subscriptions = this.subscriptionsService.findAll();
+    this.subscriptions$ = this.subscriptionsService.findAll();
   }
 
 }
--- src/app/services/subscriptions.service.ts
+++ src/app/services/subscriptions.service.ts
@@ -1,21 +1,20 @@
 import { Injectable } from '@angular/core';
 import { Subscriptions } from '../model/subscriptions';
 import { HttpClient } from '@angular/common/http';
+import { delay, first } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SubscriptionsService {
 
-  constructor(private httpCliente: HttpClient) { }
+  private readonly API_URL = ('assets/subscriptions.json')
+  constructor(private httpClient: HttpClient) { }
 
-  findAll(): Subscriptions[] {
-    return[{
-        name: 'Netflix',
-        price: 100,
-        dueDate: new Date('2025-01-01'),
-        frequency: 'Mensal'
-      }
-    ];
+  findAll() {
+    return this.httpClient.get<Subscriptions[]>(this.API_URL).pipe(
+      first(),
+      delay(5000)
+    )
   }
 }
